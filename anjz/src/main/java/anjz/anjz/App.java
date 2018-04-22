2a0ec95309164a44033b43b6af2786f684ceaea8
anjunzhou <anjunzhou@10.26.22.21>
2018-04-22 23:03:22 +0800

    初级版本
    Change-Id: I298d22e36a396bc1f7fd17035c099ebb7804a34b
    Signed-off-by: anjunzhou <anjunzhou@10.26.22.21>

---
+++ anjz/src/main/java/anjz/anjz/App.java
@@ -0,0 +1,13 @@
+package anjz.anjz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ anjz/src/main/java/anjz/anjz/UserAction.java
@@ -0,0 +1,10 @@
+package anjz.anjz;
+import java.io.UnsupportedEncodingException;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+  
+import org.apache.struts2.ServletActionContext;  
+import com.opensymphony.xwork2.ActionSupport;  
+public class UserAction extends ActionSupport{
+
+}
